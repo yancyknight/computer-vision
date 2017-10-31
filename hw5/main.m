c3747d885fda1cc1c203bfe07303f5feb0a87b65
Yancy Knight <yancy.knight@gmail.com>
2017-10-31 14:34:29 -0600

    Do 1.2.

--- hw5/main.m
+++ hw5/main.m
@@ -1,7 +1,7 @@
 % Yancy Knight A01421507
 % HW5
 
-% Problem 1.1
+% -------- Problem 1.1 --------
 cityIm = imread('City.jpg');
 squareSE = strel('square', 3);
 g = imdilate(cityIm, squareSE) - imerode(cityIm, squareSE);
@@ -11,15 +11,19 @@ disp('---------Finished Problem 1.1----------');
 pause;
 close;
 
-% Problem 1.2
+% -------- Problem 1.2 --------
 squaresIm = imread('SmallSquares.tif');
-
+se = [0, 1, 0; 
+      0, 1, 1; 
+      0, 0, 0];
+filtSquares = imerode(squaresIm, se);
+imshow(filtSquares);
 
 disp('---------Finished Problem 1.2----------');
 pause;
 close;
 
-% Problem 1.3
+% -------- Problem 1.3 --------
 wireIm = imread('Wirebond.tif');
 
 
@@ -27,7 +31,7 @@ disp('---------Finished Problem 1.3----------');
 pause;
 close;
 
-% Problem 1.4
+% -------- Problem 1.4 --------
 shapesIm = imread('Shapes.tif');
 
 
@@ -35,7 +39,7 @@ disp('---------Finished Problem 1.4----------');
 pause;
 close;
 
-% Problem 1.5
+% -------- Problem 1.5 --------
 dowelsIm = imread('Dowels.tif');
 
 
@@ -43,25 +47,25 @@ disp('---------Finished Problem 1.5----------');
 pause;
 close;
 
-% Problem 2.1
+% -------- Problem 2.1 --------
 
 disp('---------Finished Problem 2.1----------');
 pause;
 close;
 
-% Problem 2.2
+% -------- Problem 2.2 --------
 
 disp('---------Finished Problem 2.2----------');
 pause;
 close;
 
-% Problem 2.3
+% -------- Problem 2.3 --------
 
 disp('---------Finished Problem 2.3----------');
 pause;
 close;
 
-% Problem 2.4
+% -------- Problem 2.4 --------
 
 
 disp('---------Finished Problem 2.4----------');
